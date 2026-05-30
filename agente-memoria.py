770f807c1dfe6c1fe7ec9e7e2e894bc39184160b
ElHopperCBR <cesar.brandao@sp.senai.br>
2026-05-30 10:23:48 -0300

    feat: adiciona front

--- agente-memoria.py
+++ agente-memoria.py
@@ -1,4 +1,4 @@
-from agno.agent import Agent
+from agno.agent import Agent 
 from agno.models.openai import OpenAIChat
 from agno.tools.duckduckgo import DuckDuckGoTools
 from agno.tools.tavily import TavilyTools
